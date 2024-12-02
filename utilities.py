887957b2bfb387b67f200baa69e52c2ecdb98649
mattofficeuk <mattofficeuk@googlemail.com>
2024-12-02 16:41:32 +0000

    better handling of incomplete years and improved plotting

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import cdsapi
+import pandas as pd
 
-def retrieve_data(KEY, variable, date_range, lat, lng):
+def retrieve_data(KEY, variable, date_range, lat, lng, do_retrieve=True):
     # Define the URL for the CDS API
     URL = 'https://cads-mini-cci1.copernicus-climate.eu/api'
 
@@ -17,6 +18,8 @@ def retrieve_data(KEY, variable, date_range, lat, lng):
 
     # Define the filename for the data download
     timeseries_file = f"{variable}_{date_range[0]}_{date_range[1]}_{lat}_{lng}.nc"
+    if not do_retrieve:
+        return timeseries_file
 
     print(request)
     print(timeseries_file)
@@ -43,3 +46,22 @@ def cumulative_days_in_months():
         cumulative_days.append(total_days)
 
     return cumulative_days
+
+def truncate_data(var):
+    # Create a range of the final hours of each year in the dataset
+    start_year = var.valid_time.dt.year.min().item()
+    end_year = var.valid_time.dt.year.max().item()
+
+    final_hours = pd.date_range(f"{start_year}-12-31T23:00:00", 
+                                f"{end_year}-12-31T23:00:00", 
+                                freq="YE")
+
+    # Filter only the years where the final hour is in the dataset
+    valid_years = [dt.year for dt in final_hours if dt in var.valid_time]
+
+    # Select data for those years
+    var_truncated =var.sel(
+        valid_time=var.valid_time.dt.year.isin(valid_years)
+    )
+
+    return var_truncated
